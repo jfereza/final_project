42e2ed12a7dab56642e8aad6b257b1087fd9b6b9
Julia Ferrer <julia.fe.1996@gmail.com>
2024-12-02 12:38:30 +0000

    silver plant_shop done

--- models/staging/plant_shop/stg_plant_shop__products.sql
+++ models/staging/plant_shop/stg_plant_shop__products.sql
@@ -20,6 +20,7 @@ final as (
         production_price::decimal(14,3) as production_price, -- dejo 3 decimales
         price::decimal(14,3) as selling_price, -- dejo 3 decimales
         inventory::number as inventory,
+        convert_timezone('UTC', updated_at) as updated_at_utc,  -- convierto la zona horaria
         convert_timezone('UTC', dbt_valid_from) as _snp_first_ingest_utc, -- renombro la columna y convierto la zona horaria
         convert_timezone('UTC', dbt_valid_to) as _snp_invalid_from_utc, -- renombro la columna y convierto la zona horaria
         convert_timezone('UTC', _fivetran_synced) as _fivetran_synced_utc, -- convierto la zona horaria
